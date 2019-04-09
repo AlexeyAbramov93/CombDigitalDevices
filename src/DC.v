625a8dd8211230c3a2138b6b8ba45551664e2416
Alexey <alexey.abramov93@gmail.com>
2019-04-10 01:11:58 +0300

    Update DC
    Внесение изменений в модуль DC

--- src/DC.v
+++ src/DC.v
@@ -12,8 +12,9 @@ output reg [7:0] outData
 );
 
 always @(inData) begin
-
-		case (inData[2:0])	
+	outData = 8'b00000001 << inData;
+/*
+	case (inData[2:0])	
 		3'b000 :	outData = 8'b00000001;
 		3'b001 :	outData = 8'b00000010;
 		3'b010 : outData = 8'b00000100;
@@ -22,7 +23,8 @@ always @(inData) begin
 		3'b101 :	outData = 8'b00100000;
 		3'b110 :	outData = 8'b01000000;
 		3'b111 :	outData = 8'b10000000;			
-		endcase	
+	endcase
+*/
 end
 	
 endmodule
